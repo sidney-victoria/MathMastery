d587380878b30bc837d1f659868842e827992b34
sidney-victoria <sidneyvpetrulis@gmail.com>
2026-01-10 18:55:18 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,8 @@ const calculator = Desmos.GraphingCalculator(elt);
 
 calculator.setExpression({ id: "f", latex: "f(x) = ax^2" });
 calculator.setExpression({ id: "a", latex: "a = 1", sliderBounds: { min: -3, max: 3 }});
+
+// Example JS for hover glow effects or future interactivity
+// Right now all CSS handles hover
+console.log("MathMastery homepage loaded!");
+
