d08313ad7427c1e4b8f162ed99c5159608207dc7
sidney-victoria <sidneyvpetrulis@gmail.com>
2026-01-10 18:03:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const elt = document.getElementById("calculator");
 const calculator = Desmos.GraphingCalculator(elt);
 
-calculator.setExpression({
-  id: "f",
-  latex: "f(x) = x^2"
-});
+calculator.setExpression({ id: "f", latex: "f(x) = ax^2" });
+calculator.setExpression({ id: "a", latex: "a = 1", sliderBounds: { min: -3, max: 3 }});
